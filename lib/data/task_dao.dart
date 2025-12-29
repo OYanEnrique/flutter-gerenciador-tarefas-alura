42d4edcac8353c677e199d3fc988e04f23d5e13d
OYanEnrique <enrique4work@gmail.com>
2025-12-29 16:38:42 -0300

    feat: adiciona persistência de níveis no banco de dados

--- lib/data/task_dao.dart
+++ lib/data/task_dao.dart
@@ -7,14 +7,46 @@ class TaskDao {
       'CREATE TABLE $_tablename('
       '$_name TEXT, '
       '$_difficulty INTEGER, '
-      '$_image TEXT)';
+      '$_image TEXT, '
+      '$_nivel INTEGER)';
 
-  static const String _tablename = 'taskTable';
+  static const String _tablename = 'taskTable2';
   static const String _name = 'name';
   static const String _difficulty = 'difficulty';
   static const String _image = 'image';
+  static const String _nivel = 'nivel';
 
-  save(Task tarefa) async {}
+  save(Task tarefa) async {
+    print('Acessando o save');
+    final Database bancoDeDados = await getDatabase();
+    var itemExists = await find(tarefa.nome);
+    Map<String, dynamic> taskMap = toMap(tarefa);
+    if (itemExists.isEmpty) {
+      print('A tarefa não existe, salvando nova tarefa: ${tarefa.nome}');
+      return await bancoDeDados.insert(
+        _tablename,
+        taskMap,
+      );
+    } else {
+      print('A tarefa já existe, atualizando tarefa: ${tarefa.nome}');
+      return await bancoDeDados.update(
+        _tablename,
+        taskMap,
+        where: '$_name = ?',
+        whereArgs: [tarefa.nome],
+      );
+    }
+  }
+    Map<String, dynamic> toMap(Task tarefa){
+      print('Convertendo tarefa em Map: ${tarefa.nome}');
+      final Map<String, dynamic> mapaDeTarefas = {};
+      mapaDeTarefas[_name] = tarefa.nome;
+      mapaDeTarefas[_difficulty] = tarefa.dificuldade;
+      mapaDeTarefas[_image] = tarefa.foto;
+      mapaDeTarefas[_nivel] = tarefa.nivel;
+      print('Mapa de Tarefas: $mapaDeTarefas');
+      return mapaDeTarefas;
+    }
   Future<List<Task>> findAll() async {
     print('Acessando o findall');
     final Database bancoDeDados = await getDatabase();
@@ -30,6 +62,7 @@ class TaskDao {
         linha[_name],
         linha[_image],
         linha[_difficulty],
+        linha[_nivel] ?? 0,
       );
       tarefas.add(tarefa);
     }
@@ -47,5 +80,13 @@ class TaskDao {
     print('Procurando dados no banco de dados...encontrado: ${toList(result)}');
     return toList(result);
   }
-  delete(String nomeDaTarefa) async {}
+  delete(String nomeDaTarefa) async {
+    print('Deletando tarefa: $nomeDaTarefa');
+    final Database bancoDeDados = await getDatabase();
+    return bancoDeDados.delete(
+      _tablename,
+      where: '$_name = ?',
+      whereArgs: [nomeDaTarefa],
+    );
+  }
 }
